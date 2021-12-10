ce8c075167961f0d37d7328cf0a66492d367ca03
Andrey Averyanov <averyanov1996@gmail.com>
2021-12-10 13:50:42 +0300

    1

---
+++ PersonalQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionViewController.swift
+//  PersonalQuiz
+//
+//  Created by Андрей Аверьянов on 08.12.2021.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+}
+
---
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  QuestionsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Андрей Аверьянов on 10.12.2021.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var rangedStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
